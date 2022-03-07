d2777b5a485d3fddc5e02cecf5039c49f933639b
Mr. Wachs <mrLWachs@gmail.com>
2022-03-07 10:39:16 -0600

    Sample GitHub Project

--- ComputerScience32SIB/src/testing/Tester.java
+++ ComputerScience32SIB/src/testing/Tester.java
@@ -12,6 +12,21 @@ package testing;
 public class Tester 
 {
     
-    // 
+    /**
+     * A method (also thought of as an "action", "function", "procedure") is
+     * a section (module) of code that performs an action - it may need data
+     * (information) "passed" into the method (a input(s)) called a
+     * parameter(s) and it may send back information (output) which it "returns"
+     * a "special" method is written in classes called a "constructor" method
+     * that is called "invoked" or run when the class is created (making an
+     * object, "instantiated" - means make into an "instance" of that class).
+     * Constructor methods have the same name as the class (includes a capital
+     * letter at the start) - and they have NO return type (not even the word 
+     * "void") - constructor methods are "public"
+     */
+    public Tester() {
+        
+    }
+    
     
 }
