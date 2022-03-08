8fda1aa1648bcf287414b874b41750d2c262f57b
Mr. Wachs <mrLWachs@gmail.com>
2022-03-08 11:00:00 -0600

    Sample GitHub Project

--- ComputerScience32SIB/src/testing/RecursionTest.java
+++ ComputerScience32SIB/src/testing/RecursionTest.java
@@ -2,6 +2,8 @@
 /** Required package class namespace */
 package testing;
 
+import tools.Calculator;
+
  
 /**
  * RecursionTest.java - tests the concepts learned in this unit. Recursion is 
@@ -36,9 +38,27 @@ public class RecursionTest
         // called "Unit Testing" - this can be expanded on even into specific
         // programming languages (for example "JUnit")
         
+        // When testing, think of three scenarios:
+            // (1) Typical case(s) - "typical" user
+            // (2) Edge case(s)    - Go to the "edges"
+            // (3) Beyond case(s)  - Go "beyond" the edges for "bad/smart" users        
+        
+        System.out.println("Recursive factorial..............................");
+        
+        // Set up constants to set the "edges"
+        final int MIN_FACTORIAL = 1;
+        final int MAX_FACTORIAL = 5;
+        
+        // Travel ("traverse") through all the edge cases
+        for (int i = MIN_FACTORIAL; i <= MAX_FACTORIAL; i++) {
+            int number = i;
+            int answer = Calculator.factorial(number);
+            System.out.println("Number " + number + " is factorial " + answer);
+        }
         
         
         
+            
         System.out.println("Recursion test complete!");
     }   
     
--- ComputerScience32SIB/src/tools/Calculator.java
+++ ComputerScience32SIB/src/tools/Calculator.java
@@ -11,7 +11,19 @@ package tools;
  */
 public class Calculator 
 {
-    
-    
+
+    /**
+     * Calculates the factorial on a number using a recursive method. Recursive
+     * methods need at least one base case (to stop the recursion), but can 
+     * have more than one. They also need to have at minimum one recursive 
+     * case (but also can have multiple recursive cases). Recursive cases 
+     * means the method calls itself!
+     * 
+     * @param number the number to calculate the factorial of
+     * @return the factorial of the parameter
+     */
+    public static int factorial(int number) {
+        return number;
+    }
     
 }
