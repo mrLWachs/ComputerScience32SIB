1cf56566e51101535864464ca7912898dd069d1e
Mr. Wachs <mrLWachs@gmail.com>
2022-05-04 10:23:51 -0500

    Sample GitHub Project

--- ComputerScience32SIB/src/testing/unit2/Student.java
+++ ComputerScience32SIB/src/testing/unit2/Student.java
@@ -27,23 +27,80 @@ public class Student extends Person
     // and the Student class inherits all "public" properties and methods
     // from the "Person" class ("private" properties and methods are inherited,
     // but they are still private or encapsulated)
-    
+
     /**
      * Represents a book object (this object "has a" relationship (association)
      * or "usage" (or "composition") with the student class
      */
     public Book book;
-    
+
     // "number" is the name, "int" is the data type, "private" is a modifier
     // to make this "property" encapsulated - so this property or this 
     // "global variable"
-    
+
     private int number;
     private double gpa;
     
     
+    /**
+     * If a constructor has NO parameters it is called the "default" 
+     * constructor 
+     * 
+     * Default constructor method for the class, sets the class properties 
+     * (including the properties it inherited form the super or parent class)
+     */
+    public Student() {
+        super();            // invoking (calling) the "super-constructor"
+        
+        // Set the class properties (of this child/sub class of the Person 
+        // class which is the super/parent class)
+        number = 0;
+        gpa    = 0.0;
+        book   = null;
+    }
     
+    /**
+     * Overloaded constructor method (means two or more methods with the same 
+     * method name, but different parameters)
+     * 
+     * NOTE: in Java, if you create a class and do NOT write any constructors
+     * (like our "Book" class) then the IDE (NetBeans) will write a default
+     * constructor for you when the code compiles (you do not see it). However,
+     * if you write either a default or a constructor with parameters - then
+     * the IDE will NOT write any default constructor
+     * 
+     * @param name for this student
+     * @param age for this student
+     * @param gender for this student 
+     */
+    public Student(String name, int age, String gender) {
+        super(name, age, gender);   // call the second super-constructor
+        number = 1;
+        gpa    = 0.0;
+        book   = null;
+    }
     
+    /**
+     * Method just for this class, raises the average 
+     */
+    public void study() {
+        gpa *= 1.10;                            // Increase average by 10%
+    }
     
+    /**
+     * Method just for this class, lowers the student's average
+     */
+    public void slackOff() {
+        gpa = gpa - 0.05;                       // Decrease average by 5%
+    }
+    
+    /**
+     * Method just for this class, is intense repetitive studying
+     */
+    public void cram() {
+        for (int i = 0; i < 5; i++) {               // Study 5 times as hard
+            study();
+        }
+    }
     
 }
